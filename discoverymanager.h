987869db530392e6da4283433ea2e957359b0ba9
Thomas Olivares <thomasolivares@hotmail.fr>
2026-07-24 09:17:09 +0200

    done

--- discoverymanager.h
+++ discoverymanager.h
@@ -115,7 +115,7 @@ public:
     inline void setPositionDiscovery(int position) {positionDiscovery = position;}
     inline void setTempBonusRessources(int bonus) {additionalBonusRessources = bonus;}
 
-    Ressources<float> computeReductionTimeDiscovery(float bonusSpeedPercent);
+    Ressources<float> computeReductionTimeDiscovery(float bonusSpeedPercent, float bonusSpeedDiscoveryPercent);
 
 private:
     DiscoveryManager();
@@ -125,7 +125,7 @@ private:
     DiscoveryManager& operator=(const DiscoveryManager&) = delete;
 
     void computeDiscoverySpeed();
-    float computeTimeToPos16(float bonusSpeedPercent);
+    float computeTimeToPos16(float bonusSpeedPercent, float bonusSpeedDiscoveryPercent);
 
 private:
     std::map<DiscoveryManager::DiscoveryType, Discovery> dataDiscoveries;
